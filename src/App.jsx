61bbcd600ea759386e7d4d6af546c065950790a0
DFraserStratos <d.fraser@stp.co.nz>
2025-01-03 22:57:02 +1300

    Add error boundary and update App component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import DialogueExtractor from './components/DialogueExtractor'
+import React from 'react';
+import DialogueExtractor from './components/DialogueExtractor';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Error in component:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div className="p-4 text-red-500">Something went wrong. Please check the console for details.</div>;
+    }
+
+    return this.props.children;
+  }
+}
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
-      <DialogueExtractor />
-    </div>
-  )
+    <ErrorBoundary>
+      <div className="min-h-screen bg-background py-8 px-4 sm:px-6 lg:px-8">
+        <DialogueExtractor />
+      </div>
+    </ErrorBoundary>
+  );
 }
 
-export default App
+export default App;
