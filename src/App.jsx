2f21d9584b9b5f66d05485a89f92556854029c31
DFraserStratos <d.fraser@stp.co.nz>
2025-01-03 22:44:27 +1300

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DialogueExtractor from './components/DialogueExtractor'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
+      <DialogueExtractor />
+    </div>
+  )
+}
+
+export default App
