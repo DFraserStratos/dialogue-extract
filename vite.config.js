2eabc3eac6af9239d918a22375fd16eeac7cee34
DFraserStratos <d.fraser@stp.co.nz>
2025-01-04 08:29:35 +1300

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/dialogue-extract/',
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
+    sourcemap: true
+  }
 })
